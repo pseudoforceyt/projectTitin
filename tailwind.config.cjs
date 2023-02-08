d0364ea596e00336a28219ef770fdada301fd8ba
Ilamparithi M <70620481+pseudoforceyt@users.noreply.github.com>
2023-02-08 20:11:43 +0530

    theme test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,21 @@ module.exports = {
   theme: {
     extend: {}
   },
+  daisyui: {
+    themes: [
+      {pseudoforce: {
+        "primary": "#ea8c93",
+        "secondary": "#5b16bc",
+        "accent": "#e59f49",
+        "neutral": "#38233E",
+        "base-100": "#3C313F",
+        "info": "#1245ED",
+        "success": "#20A254",
+        "warning": "#F8AE25",
+        "error": "#DC384B",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")]
 };
