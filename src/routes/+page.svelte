3d0fbceb87bd7b3e4b04d227866e16a82edbb390
Ilamparithi M <5023573ilamparithi@gmail.com>
2023-03-11 02:49:55 +0530

    change

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<body>
+  <div><navbar /></div>
+  <div class="content-center"><aboutbutton /></div>
+  <script>
+    import navbar from './components/navbar.svelte';
+    import aboutbutton from './components/aboutbutton.svelte';
+  </script>
+</body>
