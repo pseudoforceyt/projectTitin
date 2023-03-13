c8acd3eaf9e6bed9bb0dc4cb8b4d5373099901e0
Ilamparithi M <5023573ilamparithi@gmail.com>
2023-03-14 01:36:55 +0530

    try a thing before slepeing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <body>
-  <div><navbar /></div>
-  <div class="content-center"><aboutbutton /></div>
   <script>
     import navbar from './navbar.svelte';
     import aboutbutton from './aboutbutton.svelte';
   </script>
+  <div><navbar /></div>
+  <div class="content-center"><aboutbutton /></div>
 </body>
